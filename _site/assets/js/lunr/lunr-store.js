8481e7491a28a1a5edc4f3fc0d6e7b49ba5bffbf
karukim <qwer4437@gmail.com>
2026-03-08 20:59:13 +0900

    내 첫 지킬 블로그 배포

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "테스트 포스팅",
+        "excerpt":"이것은 테스트 포스팅입니다. 블로그가 정상적으로 동작하는지 확인하기 위한 글입니다.  ","categories": ["test"],
+        "tags": ["sample","test"],
+        "url": "/test/test-post/",
+        "teaser": null
+      },{
+        "title": "두 번째 테스트 포스팅",
+        "excerpt":"이것은 두 번째 테스트 포스팅입니다. 여러 포스팅이 정상적으로 표시되는지 확인하기 위한 글입니다.ßß  ","categories": ["test"],
+        "tags": ["sample","second"],
+        "url": "/test/second-test-post/",
+        "teaser": null
+      }]
